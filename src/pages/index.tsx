fa1e15902741ff4a2289f6495b46202675a14184
meta-kwon <meta.kwon@meta.camp>
2023-03-24 11:34:01 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   const ogTitle = `${router.query.title}`;
   const ogDescription = `${router.query.description}`;
   const ogUrl = `${router.query.image}`;
-
+  console.log(ogTitle, ogDescription);
   const path = 'metavity';
   const param = 'dhLAheDvH6pE6Bu9d2vYXv';
   const intentUri = `intent://${path}?${param}#Intent;scheme=there_v1;package=com.metacamp.metathere;end`;
@@ -55,22 +55,24 @@ export default function Home() {
   const Index = () => {
     return (
       <>
-        <NextSeo
-          title={ogTitle}
-          description={ogDescription}
-          openGraph={{
-            type: 'website',
-            title: ogTitle,
-            description: ogDescription,
-            images: [
-              {
-                url: `/${ogUrl}`,
-                width: 800,
-                height: 400,
-              },
-            ],
-          }}
-        />
+        {ogTitle && (
+          <NextSeo
+            title={ogTitle}
+            description={ogDescription}
+            openGraph={{
+              type: 'website',
+              title: ogTitle,
+              description: ogDescription,
+              images: [
+                {
+                  url: `/${ogUrl}`,
+                  width: 800,
+                  height: 400,
+                },
+              ],
+            }}
+          />
+        )}
       </>
     );
   };
