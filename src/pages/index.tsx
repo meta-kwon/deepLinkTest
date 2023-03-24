24b4e7449e283227c408d52546fbb9560d58d417
meta-kwon <meta.kwon@meta.camp>
2023-03-24 14:09:11 +0900

    2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,41 +2,25 @@ import { NextSeo } from 'next-seo';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
-console.log('ver 4');
+console.log('ver 5');
 
 export default function Home() {
   const router = useRouter();
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [image, setImage] = useState('');
 
-  const ogTitle = `${decodeURIComponent(router.query.title as string)}`;
-  const ogDescription = `${decodeURIComponent(
-    router.query.description as string
-  )}`;
-  const ogUrl = `${router.query.image}`;
-  console.log(ogTitle, ogDescription);
   const path = 'metavity';
   const param = 'dhLAheDvH6pE6Bu9d2vYXv';
   const intentUri = `intent://${path}?${param}#Intent;scheme=there_v1;package=com.metacamp.metathere;end`;
 
-  function openAndroid() {
+  function openAndroid(code: string) {
     const path = 'metavity';
     const param = 'dhLAheDvH6pE6Bu9d2vYXv';
-    const intentUri = `intent://${path}?${param}#Intent;scheme=there_v1;package=com.metacamp.metathere;end`;
-    location.href = intentUri;
-
-    alert(`${intentUri}/초대코드: dhLAheDvH6pE6Bu9d2vYXv`);
+    const intentUri = `intent://${path}?${code}#Intent;scheme=there_v1;package=com.metacamp.metathere;end`;
     location.href = intentUri;
   }
 
   useEffect(() => {
     if (Object.keys(router.query).length > 0) {
-      const { title, description, image, code } = router.query;
-
-      setTitle(decodeURIComponent(title as string));
-      setDescription(decodeURIComponent(description as string));
-      setImage(decodeURIComponent(image as string));
+      const { code } = router.query;
 
       const userAgent = navigator.userAgent;
 
@@ -47,7 +31,7 @@ export default function Home() {
       const isDesktop = !isAndroid && !isIOS;
 
       if (isAndroid) {
-        openAndroid();
+        openAndroid(code as string);
       } else if (isDesktop) {
         location.href = `there://${path}?${code}`;
       }
@@ -56,55 +40,22 @@ export default function Home() {
 
   const Index = () => {
     return (
-      <>
-        {ogTitle && (
-          <NextSeo
-            title="there"
-            description="세상의 모든 사람들을 연결하는 새로운 가상 세계"
-            openGraph={{
-              type: 'website',
-              title: 'there',
-              description: '세상의 모든 사람들을 연결하는 새로운 가상 세계',
-              images: [
-                {
-                  url: `/there.png`,
-                },
-              ],
-            }}
-          />
-        )}
-      </>
+      <NextSeo
+        title="there"
+        description="세상의 모든 사람들을 연결하는 새로운 가상 세계"
+        openGraph={{
+          type: 'website',
+          title: 'there',
+          description: '세상의 모든 사람들을 연결하는 새로운 가상 세계',
+          images: [
+            {
+              url: `/there.png`,
+            },
+          ],
+        }}
+      />
     );
   };
 
-  return (
-    <>
-      <div
-        style={{
-          width: '100vw',
-          height: '100vh',
-          flex: 'display',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Index />
-      </div>
-    </>
-  );
-}
-
-export async function getStaticProps(context: any) {
-  const { params } = context;
-  // const { title, description, image, code } = params;
-
-  console.log(context, params);
-
-  return {
-    props: {
-      title: 1,
-      description: 1,
-      imageUrl: 1,
-    },
-  };
+  return <Index />;
 }
